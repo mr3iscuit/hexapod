0b5a4579a4d137aa2bcc70e7b49088ef477bf531
AzeRobot <eyvaz.bayramov2018@gmail.com>
2023-04-14 16:30:23 +0400

    add right side legs

---
+++ sketch_apr14a/sketch_apr14a.ino
@@ -0,0 +1,20 @@
+#include <IRremote.hpp>
+#include<Servo>
+// 3, 5, 6, 8, 9, 10, 11 (servo)
+Servo upperLeft1, upperLeft2, upperLeft3;
+Servo bottomLeft1, bottomLeft2, bottomLeft3;
+Servo lowerLeft1, lowerLeft2, lowerLeft3;
+
+Servo upperLeft1, upperLeft2, upperLeft3;
+Servo bottomLeft1, bottomLeft2, bottomLeft3;
+Servo lowerLeft1, lowerLeft2, lowerLeft3;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
